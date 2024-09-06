265afdd0ad6e556ea7b7a174b8ca2796b1b0a5b7
CrJunior08 <cleristoncontato@outlook.com>
2024-09-06 17:49:55 -0300

    resolvido 2 questão

--- Program.cs
+++ Program.cs
@@ -1,14 +1,88 @@
 ﻿using System;
-using System.Runtime.InteropServices;
-using System.Runtime.Serialization;
 
 public class Program
 {
     static void Main()
     {
-        int resultado = Soma();
+        Console.WriteLine("1 - Resultado da soma");
+        ExibirResultadoSoma();
+        Console.WriteLine("\n2 - Proximo elemento");
+        ExibirResultadoSequencias();
 
+        
+    }
+
+    static void ExibirResultadoSequencias()
+    {
+        Console.WriteLine("sequencia a: 1, 3, 5, 7, ___");
+        Console.WriteLine("Proximo elemento: " + ProximoElementoA());
+
+        Console.WriteLine("\nsequencia b: 2, 4, 8, 16, 32, 64, ____");
+        Console.WriteLine("Proximo elemento: " + ProximoElementoB());
+
+        Console.WriteLine("\nsequencia c: 0, 1, 4, 9, 16, 25, 36, ____");
+        Console.WriteLine("Próximo elemento: " + ProximoElementoC());
+
+        Console.WriteLine("\nsequencia d: 4, 16, 36, 64, ____");
+        Console.WriteLine("Proximo elemento: " + ProximoElementoD());
+
+        Console.WriteLine("\nsequencia e: 1, 1, 2, 3, 5, 8, ____");
+        Console.WriteLine("Proximo elemento: " + ProximoElementoE());
+
+        Console.WriteLine("\nsequencia f: 2, 10, 12, 16, 17, 18, 19, ____");
+        Console.WriteLine("Proximo elemento: " + ProximoElementoF());
+    }
+
+    static void ExibirResultadoSoma()
+    {
+        int resultado = Soma();
+        Console.WriteLine("\nResultado da soma:");
         Console.WriteLine("Resultado final: " + resultado);
+    }
+
+    static int ProximoElementoA()
+    {
+        int ultimo = 7;
+        return ultimo + 2;
+    }
+
+    static int ProximoElementoB()
+    {
+        int[] sequencia = { 2, 4, 8, 16, 32, 64 };
+        return sequencia[sequencia.Length - 1] * 2;
+    }
+
+    static int ProximoElementoC()
+    {
+        int proximoNumero = 7; 
+        return proximoNumero * proximoNumero;
+    }
+
+    static int ProximoElementoD()
+    {
+        int[] sequencia = { 4, 16, 36, 64 };
+        int proximoIndice = (int)Math.Sqrt(sequencia[sequencia.Length - 1]) + 2;
+        return proximoIndice * proximoIndice;
+    }
+
+    static int ProximoElementoE()
+    {
+        int a = 1;
+        int b = 1; 
+        int c;
+        for (int i = 2; i < 7; i++)
+        {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
+
+    static int ProximoElementoF()
+    {
+        int[] sequencia = { 2, 10, 12, 16, 17, 18, 19 };
+        return sequencia[sequencia.Length - 1] + 1;
     }
 
     static int Soma()
@@ -25,5 +99,4 @@ public class Program
         }
         return soma;
     }
-
 }
