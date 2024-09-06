5e8e491bec4f636c9b1fa149389add3d31b30bb1
CrJunior08 <cleristoncontato@outlook.com>
2024-09-06 17:10:07 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
+
+public class Program
+{
+    static void Main()
+    {
+        int resultado = Soma();
+
+        Console.WriteLine("Resultado final: " + resultado);
+    }
+
+    static int Soma()
+    {
+        int indice = 12;
+        int soma = 0;
+        int k = 1;
+
+        while (k < indice)
+        {
+            k = k + 1;
+            soma += k;
+            Console.WriteLine(soma);
+        }
+        return soma;
+    }
+
+}
